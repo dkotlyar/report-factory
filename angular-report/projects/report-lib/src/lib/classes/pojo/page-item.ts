2a00291911e8ed9b30a0d6dbd11a5492ad10dedb
Dmitry Kotlyar <33775104+dkotlyar@users.noreply.github.com>
2021-01-09 12:59:04 +0300

    ????????? ???????????? ?? ??????????

--- angular-report/projects/report-lib/src/lib/classes/pojo/page-item.ts
+++ angular-report/projects/report-lib/src/lib/classes/pojo/page-item.ts
@@ -1,8 +1,13 @@
 import {ElementRef} from '@angular/core';
 
 export class PageItem {
+  // Содержит элемент массива DATA, переданный в качестве параметра в конструктор PagesFactory.
   content: any;
+
+  // Содержит вычисленную высоту элемента при генерировании отчёта
   height: number;
+
+  // Содержит индекс страницы, на которой данный объект располагается.
   pageNum: number;
 
   constructor(content: any) {
--- angular-report/projects/report-lib/src/lib/classes/pojo/page.ts
+++ angular-report/projects/report-lib/src/lib/classes/pojo/page.ts
@@ -2,12 +2,22 @@ import {PageItem} from './page-item';
 import {ElementRef, EventEmitter, QueryList, ViewChild, ViewChildren} from '@angular/core';
 
 export class Page {
+  // Поле content содержит произвольный объект, переданный в качестве параметра CONTENT в конструктор PagesFactory.
   content: any;
-  items: Array<PageItem> = [];
-  freeHeight: number;
-  contentHeight: number;
-  headerAndFooterHeight: number;
 
+  // Поле items содержит массив PageItem, хранящийся для данной страницы
+  items: Array<PageItem> = [];
+
+  // Поле freeHeight содержит высоту свободного места на странице.
+  // Может быть использовано в шаблоне для заполнения свободного пространства.
+  freeHeight: number;
+
+  // Поле определяет доступную высоту страницы для генерирования отчёта. Вычисляется автоматически для элемента page
+  contentHeight: number;
+  // Поле содержит высоту элементов header, footer, а также требуемое свободное место на странице
+  headerAndFooterHeight: number;
+  // Требуемое свободное место на странице. Задается в пикселях.
+  // Read-Write.
   minimumFreeHeight: number;
 
   page: ElementRef;
