7e8f81c15fd975d4693c79f4868091a1afc005e8
Dmitry Kotlyar <33775104+dkotlyar@users.noreply.github.com>
2021-01-12 17:35:31 +0300

    ???????? ?????? ??? ????????????? ?????????? ??????? ????????????

--- angular-report/src/app/reports/spec-report/spec-report/spec-report.component.ts
+++ angular-report/src/app/reports/spec-report/spec-report/spec-report.component.ts
@@ -1,31 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SpecService} from '../../../services/spec.service';
 import {PagesFactory} from 'report-lib';
 import {HttpErrorResponse} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-spec-report',
-  templateUrl: './spec-report.component.html',
-  styleUrls: ['./spec-report.component.css']
+    selector: 'app-spec-report',
+    templateUrl: './spec-report.component.html',
+    styleUrls: ['./spec-report.component.css']
 })
 export class SpecReportComponent implements OnInit {
 
-  pagesFactory: PagesFactory;
-  error: HttpErrorResponse;
+    pagesFactory: PagesFactory;
+    error: HttpErrorResponse;
 
-  constructor(private spec: SpecService) { }
+    constructor(private spec: SpecService,
+                private activatedRoute: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-    this.spec.getLocalReportData('gt3660300')
-        .then(data => {
-          this.pagesFactory = new PagesFactory(data);
-          this.pagesFactory.splitPages();
-        })
-        .catch(e => this.error = e);
-  }
+    ngOnInit(): void {
+        const id = this.activatedRoute.snapshot.params.id;
 
-  get complited(): boolean {
-    return this.pagesFactory.complited;
-  }
+        this.spec.getReportData(id)
+            .then(data => {
+                this.pagesFactory = new PagesFactory(data);
+                this.pagesFactory.splitPages();
+            })
+            .catch(e => this.error = e);
+    }
+
+    get complited(): boolean {
+        return this.pagesFactory.complited;
+    }
 
 }
