73dfadd552381a8802c927c0aabb01ae615b6d78
Dmitry Kotlyar <33775104+dkotlyar@users.noreply.github.com>
2021-01-12 19:56:34 +0300

    ???????? ????? ?????? ????????????

--- angular-report/src/app/reports/spec-report/spec-test/spec-test.component.ts
+++ angular-report/src/app/reports/spec-report/spec-test/spec-test.component.ts
@@ -1,31 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {PagesFactory} from "report-lib";
-import {HttpErrorResponse} from "@angular/common/http";
-import {SpecService} from "../../../services/spec.service";
+import {Component, OnInit} from '@angular/core';
+import {PagesFactory} from 'report-lib';
+import {HttpErrorResponse} from '@angular/common/http';
+import {SpecService} from '../../../services/spec.service';
 
 @Component({
-  selector: 'app-spec-test',
-  templateUrl: './spec-test.component.html',
-  styleUrls: ['./spec-test.component.css']
+    selector: 'app-spec-test',
+    templateUrl: './spec-test.component.html',
+    styleUrls: ['./spec-test.component.css', '../../../app.component.css']
 })
 export class SpecTestComponent implements OnInit {
 
-  pagesFactory: PagesFactory;
-  error: HttpErrorResponse;
+    pagesFactory: PagesFactory;
+    error: HttpErrorResponse;
 
-  constructor(private spec: SpecService) { }
+    constructor(private spec: SpecService) {
+    }
 
-  ngOnInit(): void {
-    this.spec.getLocalReportData('gt3660300')
-        .then(data => {
-          this.pagesFactory = new PagesFactory(data);
-          this.pagesFactory.splitPages();
-        })
-        .catch(e => this.error = e);
-  }
+    ngOnInit(): void {
+        this.spec.getLocalReportData('gt3660300')
+            .then(data => {
+                const content = {
+                    NAME: data[0].SPEC_NMK_NAME,
+                    NOTE: data[0].SPEC_NMK_NOTE
+                };
+                this.pagesFactory = new PagesFactory(data, content);
+                this.pagesFactory.splitPages();
+            })
+            .catch(e => this.error = e);
+    }
 
-  get complited(): boolean {
-    return this.pagesFactory.complited;
-  }
+    get complited(): boolean {
+        return this.pagesFactory.complited;
+    }
 
 }
