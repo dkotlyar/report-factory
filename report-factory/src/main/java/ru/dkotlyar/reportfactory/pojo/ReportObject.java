4832ad51eed9421c6c822d5e1d42d29ce4669fac
Dmitry Kotlyar <dkotlyar@vsplatforma.ru>
2021-04-19 11:07:10 +0300

    ????????? ?????????? ???????????? ??????

--- report-factory/src/main/java/ru/dkotlyar/reportfactory/pojo/ReportObject.java
+++ report-factory/src/main/java/ru/dkotlyar/reportfactory/pojo/ReportObject.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +16,8 @@ public class ReportObject implements Serializable {
     private String fileName;
     private ReportState state = ReportState.New;
     private String error;
+    private Date startGenerating;
+    private Date endGenerating;
 
     @Id
     @GeneratedValue(generator="system-uuid")
@@ -66,6 +69,12 @@ public class ReportObject implements Serializable {
 
     public void setState(ReportState state) {
         this.state = state;
+
+        if (state == ReportState.Generating) {
+            this.setStartGenerating(new Date());
+        } else if (state == ReportState.Error || state == ReportState.Done) {
+            this.setEndGenerating(new Date());
+        }
     }
 
     public String getError() {
@@ -76,28 +85,47 @@ public class ReportObject implements Serializable {
         this.error = error;
     }
 
+    public Date getStartGenerating() {
+        return startGenerating;
+    }
+
+    public void setStartGenerating(Date startGenerating) {
+        this.startGenerating = startGenerating;
+    }
+
+    public Date getEndGenerating() {
+        return endGenerating;
+    }
+
+    public void setEndGenerating(Date endGenerating) {
+        this.endGenerating = endGenerating;
+    }
+
+    @Transient
+    public Long getGeneratingTime() {
+        if (this.startGenerating == null || this.endGenerating == null) {
+            return Long.valueOf(0);
+        }
+
+        return this.endGenerating.getTime() - this.startGenerating.getTime();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ReportObject that = (ReportObject) o;
-        return Objects.equals(uuid, that.uuid) &&
-                Objects.equals(type, that.type) &&
-                Objects.equals(template, that.template) &&
-                Objects.equals(blob, that.blob) &&
-                Objects.equals(fileName, that.fileName) &&
-                state == that.state &&
-                Objects.equals(error, that.error);
+        return Objects.equals(uuid, that.uuid) && Objects.equals(type, that.type) && Objects.equals(template, that.template) && Objects.equals(blob, that.blob) && Objects.equals(fileName, that.fileName) && state == that.state && Objects.equals(error, that.error) && Objects.equals(startGenerating, that.startGenerating) && Objects.equals(endGenerating, that.endGenerating);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uuid, type, template, blob, fileName, state, error);
+        return Objects.hash(uuid, type, template, blob, fileName, state, error, startGenerating, endGenerating);
     }
 
     @Override
     public String toString() {
-        return "ReportRequest{" +
+        return "ReportObject{" +
                 "uuid='" + uuid + '\'' +
                 ", type='" + type + '\'' +
                 ", template='" + template + '\'' +
@@ -105,6 +133,8 @@ public class ReportObject implements Serializable {
                 ", fileName='" + fileName + '\'' +
                 ", state=" + state +
                 ", error='" + error + '\'' +
+                ", startGenerating=" + startGenerating +
+                ", endGenerating=" + endGenerating +
                 '}';
     }
 }
